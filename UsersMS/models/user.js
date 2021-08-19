c6e4b5a1303835a7a6431d3f895ee48e83a862a1
Ahmad Saleh <ahmadmsa87@gmail.com>
2021-08-19 23:45:26 +0400

    Update user validations

--- UsersMS/models/user.js
+++ UsersMS/models/user.js
@@ -28,6 +28,7 @@ const userSchema = new mongoose.Schema({
         required: false,
         minlength: 1,
         maxlength: 10,
+        enum: ["male", "female"],
     },
     phone: {
         type: String,
@@ -40,6 +41,7 @@ const userSchema = new mongoose.Schema({
         required: false,
         minlength: 5,
         maxlength: 20,
+        enum: ["admin", "client"],
     },
     isAdmin: Boolean,
 });
@@ -58,9 +60,9 @@ function validateUser(user) {
         name: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(255).required(),
-        gender: Joi.string().min(1).max(10).optional(),
+        gender: Joi.string().min(1).max(10).valid("male", "female").optional(),
         phone: Joi.string().min(5).max(20).optional(),
-        role: Joi.string().min(5).max(20).optional(),
+        role: Joi.string().min(5).max(20).valid("admin", "client").optional(),
     };
 
     return Joi.validate(user, schema);
